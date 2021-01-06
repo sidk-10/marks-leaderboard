2daf7e4a71bbdd67868b6556a2c3d849249cfbee
Siddharth Kulkarni <10siddharthk@gmail.com>
2021-01-06 17:10:11 +0530

    Deploy frontend to netlify

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, template_folder="./views/public", static_folder="./views/public", static_url_path='')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return app.send_static_file("index.html")
+    return "Up and running!"
 
 if __name__ == "__main__":
     app.run(debug=True)
