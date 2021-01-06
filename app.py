879d22cf7a1aea6058de2950801bb1970266c8c6
Siddharth Kulkarni <siddharthkulkarni@Siddharths-MacBook-Air.local>
2021-01-06 14:13:21 +0530

    Fix bugs

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder="./views/public", static_folder="./views/p
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return app.send_static_file("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
