7739b381fa8794df28113eee5e9fc6f6be0ae54d
Siddharth Kulkarni <siddharthkulkarni@Siddharths-MacBook-Air.local>
2021-01-06 13:10:14 +0530

    Fix bugs

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import os
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, static_folder="./views/public", static_url_path='')
+app = Flask(__name__, template_folder="./views/public", static_folder="./views/public", static_url_path='')
 
 @app.route('/')
 def index():
-    return app.send_static_file("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
